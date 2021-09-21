598da275445ca10162b1c94ed2daf0e2d91b480f
diogoMalfatti <diimalfatti@gmail.com>
2021-09-21 19:32:29 -0300

    create build front , adjust back for deploy

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 require('dotenv').config()
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const api = require('./backend/routes')
 
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.json({
-    success: true,
-  })
-})
+// app.get('/', (req, res) => {
+//   res.json({
+//     success: true,
+//   })
+// })
 
 app.use('/api', api)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'))
+
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT
 app.listen(PORT)
