5ee06417fafb4e025d7a34ea47d24fc7fc88c665
diogoMalfatti <diimalfatti@gmail.com>
2021-09-21 20:17:10 -0300

    fixing script

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const api = require('./backend/routes')
 
 const app = express()
 app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 // app.get('/', (req, res) => {
 //   res.json({
